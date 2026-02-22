f948261c38edbfc47df3fa3900f28ad2ad2a7b28
tejasM17 <tejasm9380@gmail.com>
2026-02-22 20:00:46 +0530

    Add Binary fixed

--- src/main/kotlin/feb/add binaray.kt
+++ src/main/kotlin/feb/add binaray.kt
@@ -1,11 +1,30 @@
 package feb
 
-fun addBinary(a: String, b: String) {
-    val sum = a.toInt(2) + b.toInt(2)
-    println(Integer.toBinaryString(sum))
+fun addBinary(a: String, b: String): String {
+//    val sum = a.toInt(2) + b.toInt(2)
+//    println(Integer.toBinaryString(sum))
+
+    var i = a.length - 1
+    var j = b.length - 1
+    var carry = 0
+
+    val res = StringBuilder()
+
+    while (i >= 0 || j >= 0 || carry != 0) {
+        val BitA = if (i >= 0) a[i] - '0' else 0
+        val BitB = if (j >= 0) b[j] - '0' else 0
+
+        val sum = BitA + BitB + carry
+        res.append(sum % 2)
+        carry = sum / 2
+
+        i--
+        j--
+    }
+    return res.reverse().toString()
 }
 
 
 fun main() {
-    addBinary("1010", "1011")
+    println(addBinary("1010", "1011"))
 }
