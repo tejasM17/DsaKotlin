50ad28345c395734422db42f6881e801a6aed10f
tejasM17 <tejasm9380@gmail.com>
2026-09-26 20:14:14 +0530

    Buy sell stock

--- src/main/kotlin/sept/BuySellStock.kt
+++ src/main/kotlin/sept/BuySellStock.kt
@@ -1,2 +1,26 @@
 package org.example.sept
 
+import kotlin.math.max
+
+class BuyAndSellStock {
+    fun maxProfit(prices: IntArray): Int {
+        var minPrice = prices[0]
+        var maxProfit = 0
+
+        for (price in prices) {
+            println(price)
+            if (price < minPrice){
+                minPrice = price
+            }
+            val profit = price - minPrice
+            maxProfit = maxOf(maxProfit,profit)
+        }
+        return maxProfit
+    }
+}
+
+fun main() {
+ val array = intArrayOf(7,1,5,3,6,4)
+    val arr = BuyAndSellStock()
+    println("Max Profit : ${arr.maxProfit(array)}")
+}
